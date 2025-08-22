b5812abebafd273ccacdaf7e209039ca0bfb8009
Adhithyan006 <kit27.am02@gmail.com>
2025-08-22 09:54:29 +0530

    Drum

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,63 @@
+// practice file
+
+let keys = document.querySelectorAll("button");
+
+for(let i=0;i<keys.length;i++){
+    keys[i].addEventListener("click", () => {
+         // alert('clicked'+keys[i].textContent);
+         makeSound(keys[i].textContent);
+            buttonAnimation(keys[i].textContent);
+    });
+}    
+
+document.addEventListener("keydown", function(event) {
+   // alert(event.key);
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
+ 
+
+
+function makeSound(keys){
+    switch(keys){
+        case "w":
+            var audio1 = new Audio("sounds/tom-1.mp3");
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio("sounds/tom-2.mp3");
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio("sounds/tom-3.mp3");
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio("sounds/tom-4.mp3");
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio("sounds/snare.mp3");
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio("sounds/crash.mp3");
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio("sounds/kick-bass.mp3");
+            audio7.play();
+            break;
+        default:
+            console.log(keys);
+    }
+}
